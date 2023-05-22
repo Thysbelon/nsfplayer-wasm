bdf5668a46e9ca84968d8580f7d532a38c3fcbc1
Thysbelon <98119382+Thysbelon@users.noreply.github.com>
2023-05-22 14:44:22 -0400

    Fix bug that prevented use of both Panning and Looping

--- nsfplay-function.js
+++ nsfplay-function.js
@@ -7,9 +7,13 @@ async function nsfplay(url, tracknum, settingsObject /*contains introEnd (millis
 	*/
 	// to do, use wasm workers or something to do smart multi threading
 	if (settingsObject) {
-		if (settingsObject.panningObject) {
+		console.log(settingsObject)
+		if (settingsObject.panningObject!=null) {
+			console.log("settingsObject.panningObject exists")
 			var panningObject=settingsObject.panningObject
-		} else if (settingsObject.introEnd) {
+		}  // DO NOT WRITE ELSE IF I WANT IT TO BE POSSIBLE FOR BOTH CONDITIONS TO BE FULFILLED AT THE SAME TIME
+		if (settingsObject.introEnd!=null) {
+			console.log("settingsObject.introEnd exists")
 			var introEnd=settingsObject.introEnd
 		}
 	}
@@ -154,9 +158,11 @@ async function nsfplay(url, tracknum, settingsObject /*contains introEnd (millis
 	}
 	
 	var sourceNodeSettingsObject;
-	if (introEnd) {
+	if (introEnd!=null) {
+		console.log("setting loop...")
 		sourceNodeSettingsObject={loop:true, loopEnd:nsfLength/1000, loopStart:introEnd/1000, channelCount:1}
 	} else {
+		console.log("no introEnd")
 		sourceNodeSettingsObject={channelCount:1}
 	}
 	
@@ -199,46 +205,3 @@ async function nsfplay(url, tracknum, settingsObject /*contains introEnd (millis
 		console.log("started")
 	}
 }
-
-async function nsfplay_scriptnode(url, tracknum) { // Most of this code is copied from Chip Player JS
-	var response = await fetch(url)
-	
-	const audioCtx=new AudioContext({latencyHint:"playback",sampleRate:44100});
-	
-	const bufferSize = Math.max( // Make sure script node bufferSize is at least baseLatency
-		Math.pow(2, Math.ceil(Math.log2((audioCtx.baseLatency || 0.001) * audioCtx.sampleRate))), 2048);
-	console.log(bufferSize);
-	
-	var audioNode=audioCtx.createScriptProcessor(bufferSize,2,2)
-	audioNode.connect(audioCtx.destination)
-	
-	response = await response.arrayBuffer()
-	var data=new Uint8Array(response)
-	
-	Module.ccall(
-		"setupNSF",
-		"number",
-		["array", "number", "number", "number"],
-		[data, data.length, bufferSize, tracknum/* track number */]
-	)
-	var playNSF=Module.cwrap("playNSF", "number", null)
-	audioNode.onaudioprocess=function(e){
-		//console.log("gme_play"); 
-		
-		let numchannels=e.outputBuffer.numberOfChannels
-		
-		let bufPtr=playNSF()
-		
-		let outputData=[];
-		
-		for(let channel=0; channel<numchannels; channel++){
-			outputData[channel]=e.outputBuffer.getChannelData(channel); /*nested loop*/
-			for(let i=0; i<bufferSize; i++){
-				outputData[channel][i] = Module.getValue(bufPtr+i * 2 * numchannels + /* frame offset * bytes per sample * num channels + */ channel * 2  /* channel offset * bytes per sample */, 'i16') / INT16_MAX // convert int16 to float
-			}
-		}
-		
-	}
-	
-}
-
